a9e27e045865a0c22dcd66732c2ef5f15fb0335d
Pierre <pierremarais67@gmail.com>
2023-10-08 21:40:32 +0200

    Create pretty max chunk size input field

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -4,13 +4,13 @@ import React, { useEffect, useState } from "react";
 import { convertFileToStringArray } from "../libs/compression/Conversions";
 import { DebounceInput } from "react-debounce-input";
 import BarcodeLooper from "../components/BarcodeLooper";
-import { Input } from "@material-tailwind/react";
+import { Input, Typography } from "@material-tailwind/react";
 
 
 const Upload = () => {
     const [file, setFile] = useState<File | null>(null);
     const [fileAsStringArray, setFileAsStringArray] = useState<string[] | null>();
-    const [displayTime, setDisplayTime] = useState(1000);//Math.trunc(1000 / 28));
+    const [displayTime, setDisplayTime] = useState(Math.trunc(1000 / 28));
     const [maxChunkSize, setMaxChunkSize] = useState(450);
     const [sliderValue, setSliderValue] = useState<number[]>([1, 1]);
 
@@ -30,41 +30,81 @@ const Upload = () => {
 
 
     return (
-        <div className="flex flex-col items-center">
-            <label htmlFor="file-upload">Choose a file to upload:</label>
-            <input
-                id="file-upload"
-                type="file"
-                onChange={handleFileChange}
-            />
-            {file && <p>Selected file: {file.name}</p>}
-            {file && <label htmlFor="max-chunk-size">File chunk size:</label> }
-            {file && <DebounceInput element="input" type="number" label="Max Chunk Size" id="max-chunk-size" value={maxChunkSize}
-                    minLength={3}
-                    debounceTimeout={500}
-                    onChange={event => {
-                        const chunkSize = parseInt(event.target.value);
-                        if (chunkSize <= 1000) {
-                            console.log('setting max chunk size to ' + chunkSize);
-                            setMaxChunkSize(chunkSize);
-                        }
-                    }} />
-                }
-                <br />
-            {file && <label htmlFor="qr-display-time">Display Rate (fps): </label> }
-            {file && <DebounceInput element="input"  type="number" label="Display Rate (fps): " id="qr-display-time" value={Math.trunc(1000 / displayTime)}
-                maxLength={3}
-                debounceTimeout={500}
-                onChange={event => {
-                    const time = parseInt(event.target.value);
-                    setDisplayTime(Math.trunc(1000 / time));
-                }
-                } />
-            }
-            {file && <BarcodeLooper fileDataArr={fileAsStringArray || null} displayTime={displayTime} displayRange={sliderValue}></BarcodeLooper>}
+            <div className="flex flex-col items-center">
+                <h2>Choose a file to upload:</h2>
+                <input
+                    id="file-upload"
+                    type="file"
+                    onChange={handleFileChange}
+                />
+                {file && <p>Selected file: {file.name}</p>}
 
-        </div>
-    );
+                {file && (
+                    <div className="flex items-center">
+                        <label htmlFor="qr-display-time" className="mr-4">Display Rate (fps): </label>
+                        <DebounceInput
+                            element="input"
+                            type="number"
+                            label="Display Rate (fps): "
+                            id="qr-display-time"
+                            value={Math.trunc(1000 / displayTime)}
+                            maxLength={3}
+                            debounceTimeout={500}
+                            onChange={(event) => {
+                                const time = parseInt(event.target.value);
+                                setDisplayTime(Math.trunc(1000 / time));
+                            }}
+                            className="w-20"
+                        />
+                    </div>
+                )}
+
+                
+                {file && (
+                    <div className="flex items-center">
+                        <div className="w-[32rem]">
+                            <Input 
+                                type="number"
+                                label="Max Chunk Size"
+                                id="max-chunk-size" 
+                                value={maxChunkSize}
+                                minLength={3}
+                                onChange={(event) => {
+                                    const chunkSize = parseInt(event.target.value);
+                                    if (chunkSize <= 1000) {
+                                        console.log("setting max chunk size to " + chunkSize);
+                                        setMaxChunkSize(chunkSize);
+                                    }
+                                }}
+                            />
+                            <Typography
+                                variant="small"
+                                color="gray"
+                                className="mt-2 flex items-center gap-1 font-normal"
+                            >
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="-mt-px h-4 w-4"
+                                >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                                    clipRule="evenodd"
+                                />
+                                </svg>
+                                Changing this value will change the amount of data inside each barcode.
+                            </Typography>
+                            </div>
+                        
+                    </div>
+                )}
+                
+                {file && <BarcodeLooper fileDataArr={fileAsStringArray || null} displayTime={displayTime} displayRange={sliderValue}></BarcodeLooper>}
+
+            </div>
+        );
 };
 
 export default Upload;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+
+const config: Config = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,5 +19,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
+});
 export default config
